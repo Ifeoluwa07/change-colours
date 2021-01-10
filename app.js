d27495ea6e59365c49a5e77912e25ab322396a91
Ifeoluwa07 <76498783+Ifeoluwa07@users.noreply.github.com>
2021-01-10 21:56:38 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+document.querySelector('#red').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'red';
+}
+document.querySelector('#orange').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'orange';
+}
+document.querySelector('#indigo').onclick = function (){
+  document.querySelector('body').style.backgroundColor = 'indigo';
+}
+document.querySelector('#yellow').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'yellow';
+}
+document.querySelector('#blue').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'blue';
+}
+document.querySelector('#green').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'green';
+}
+document.querySelector('#brown').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'brown';
+}
+document.querySelector('#purple').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'purple';
+}
+document.querySelector('#pink').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'pink';
+}
+document.querySelector('#grey').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'grey';
+}
+document.querySelector('#green').onclick = function (){
+    document.querySelector('body').style.backgroundColor = 'green';
+}
+
+
+
+
